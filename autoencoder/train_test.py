2590ee54ad1095311e7a312dfd33486cb2704c1b
Akhila Ananthram <asa225@cornell.edu>
2016-05-19 12:42:19 -0400

    train hsv net

--- autoencoder/train_test.py
+++ autoencoder/train_test.py
@@ -3,6 +3,7 @@ import atexit
 import caffe
 import numpy as np
 import warnings
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run train and test")
@@ -12,6 +13,10 @@ if __name__ == "__main__":
                         help="Path to pretrained caffe model")
     parser.add_argument("--niter", default=350000, type=int,
                         help="Number of iterations for training")
+    parser.add_argument("--blob-file", default=None, type=str,
+                        help="Path to blob values")
+    parser.add_argument("--train", dest="train", default=None, type=str,
+                        help="Path to train file")
     args = parser.parse_args()
 
     # Uncomment when training with a GPU
@@ -41,9 +46,34 @@ if __name__ == "__main__":
 
     atexit.register(save_loss)
 
+    blobs = {}
+    if args.blob_file is not None:
+        with open(args.blob_file) as f:
+            for line in f:
+                label, xys = line.strip().split(",")
+                blobs[label] = [float(x) for x in xys.split(" ")]
+
+    # TODO: iterate over train file to get order of images
+    train_labels = []
+    if args.train is not None:
+        with open(args.train) as f:
+            for line in f:
+                line = line.strip()
+                filename, _ = line.split(" ")
+                label, _ = os.path.splitext(os.path.basename(filename))
+                train_labels.append(label)
+
     for it in xrange(args.niter):
         if it % 1000 == 0:
             print "Iteration {}".format(it)
+        batch_blobs = []
+        for l in train_labels[it * 64: (it + 1) * 64]:
+            if l in blobs:
+                batch_blobs.append(blobs[l])
+
+        if len(batch_blobs) > 0:
+            solver.net.blobs["hsv_encoding"].data[...] = np.array(batch_blobs)
+
         solver.step(1)  # SGD by Caffe
 
         # store the train loss
