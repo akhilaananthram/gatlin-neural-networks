835ad2c803a4e39203362e2bb353582ba1782225
Akhila Ananthram <asa225@cornell.edu>
2016-05-19 12:42:09 -0400

    hsv net

--- autoencoder/preprocess/setup_net.py
+++ autoencoder/preprocess/setup_net.py
@@ -8,17 +8,18 @@ from caffe import layers as L
 from caffe import params as P
 from constants import *
 
-def add_data_layer(n, data, batch_size, phase, data_type, mean_file, blob_name="original"):
+def add_data_layer(n, data, batch_size, phase, data_type, mean_value, blob_name="original",
+                   size=[3, SIZE, SIZE], shuffle=True):
     # TODO: divide by STD
     # subtract the mean
     transform_param = {}
-    if mean_file is not None:
-        transform_param["mean_file"] = mean_file
+    if mean_value is not None:
+        transform_param["mean_value"] = mean_value
 
     # labels are not used because this is an autoencoder
     if data_type == "txt":
         n[blob_name], n.labels = L.ImageData(source=data, batch_size=batch_size, ntop=2,
-                                             new_height=SIZE, new_width=SIZE, shuffle=True,
+                                             new_height=SIZE, new_width=SIZE, shuffle=shuffle,
                                              include={"phase": phase}, mirror=(not phase),
                                              transform_param=transform_param)
     elif data_type == "db":
@@ -27,7 +28,8 @@ def add_data_layer(n, data, batch_size, phase, data_type, mean_file, blob_name="
                               include={"phase": phase},
                               transform_param=transform_param)
     else:
-        n[blob_name] = L.DummyData(shape={"dim":[batch_size, 3, SIZE, SIZE]},
+        shape = [batch_size] + size
+        n[blob_name] = L.DummyData(shape={"dim":shape},
                                    dummy_data_param={"data_filler": {"type": "xavier"}},
                                    ntop=1)
 
@@ -62,10 +64,10 @@ def add_intermediate_layers(n):
                                           python_param={"module": "probability_distribution",
                                                         "layer": "ProbabilityDistribution"})
 
-def gps_net(images, batch_size, phase, data_type, mean_file):
+def gps_net(images, batch_size, phase, data_type, mean_value):
     n = caffe.NetSpec()
 
-    add_data_layer(n, images, batch_size, phase, data_type, mean_file)
+    add_data_layer(n, images, batch_size, phase, data_type, mean_value)
 
     # Get black and white
     n.blackandwhite = L.Python(n.original, name="blackandwhite", ntop=1,
@@ -86,15 +88,19 @@ def gps_net(images, batch_size, phase, data_type, mean_file):
 
     return str(n.to_proto())
 
-def hsv_net(images, batch_size, phase, data_type, mean_file, hsv_file):
+def hsv_net(images, batch_size, phase, data_type, mean_value, hsv_file=None):
+    num_blobs = 2
     n = caffe.NetSpec()
 
-    add_data_layer(n, images, batch_size, phase, data_type, mean_file)
-    add_data_layer(n, hsv_file, batch_size, phase, data_type, mean_file, "hsv_encoding")
+    add_data_layer(n, images, batch_size, phase, data_type, mean_value, shuffle=False)
+    add_data_layer(n, hsv_file, batch_size, phase, "TODO", None, "hsv_encoding", size=[num_blobs*2])
 
     add_intermediate_layers(n)
+    n.blob_points = L.InnerProduct(n.probabilitydist, num_output=num_blobs*2,
+                                      weight_filler={"type": "xavier"},
+                                      bias_filler={"type": "constant"})
 
-    n.loss = L.EuclideanLoss(n.probabilitydist, n.hsv_encoding)
+    n.loss = L.EuclideanLoss(n.blob_points, n.hsv_encoding)
 
     return str(n.to_proto())
 
@@ -114,13 +120,19 @@ if __name__ == "__main__":
     parser.add_argument("--train-proto", default="train.prototxt", help="path to train proto file")
     parser.add_argument("--val-proto", default="val.prototxt", help="path to val proto file")
     parser.add_argument("--solver", default="solver.prototxt", help="path to solver proto file")
-    parser.add_argument("--mean", default=None, help="path to mean binary proto file")
+    parser.add_argument("--mean", default=None, type=int, help="mean value")
+    parser.add_argument("--arch", default="gps", choices=["gps", "hsv"], help="Type of architecture to make")
     args = parser.parse_args()
 
+    if args.arch == "gps":
+        net_maker = gps_net
+    else:
+        net_maker = hsv_net
+
     with open(args.train_proto, "w") as f:
-        f.write(gps_net(args.train, BATCH_TRAIN, 0, get_data_type(args.train), args.mean))
+        f.write(net_maker(args.train, BATCH_TRAIN, 0, get_data_type(args.train), args.mean))
     with open(args.val_proto, "w") as f:
-        f.write(gps_net(args.val, BATCH_TEST, 1, get_data_type(args.val), args.mean))
+        f.write(net_maker(args.val, BATCH_TEST, 1, get_data_type(args.val), args.mean))
 
     solver = caffetools.CaffeSolver(net_prototxt_path=args.train_proto, testnet_prototxt_path=args.val_proto)
     solver.sp["test_iter"] = "1000"
